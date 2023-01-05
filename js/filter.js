a5dcb0b1e1995cd9e2a01685660b2a975f9569d9
Dakusy <dakusy@gmail.com>
2023-01-05 23:18:45 +0100

    finally add algorithm search text

---
+++ js/filter.js
@@ -0,0 +1,14 @@
+const searchBar = document.querySelector('.search-bar')
+
+const filters = {
+    textInputed  : "",
+    ingredients  : [],
+    ustensils  : [],
+    appliances  : []
+}
+
+searchBar.addEventListener('input', (event) => {
+    //TODO
+})
+
+
